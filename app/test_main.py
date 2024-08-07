b723306f64b889b953808c902b80a25f32bcb64c
Yakiv Lobach <lobachyakiv@gmail.com>
2024-08-07 20:24:11 +0300

    first_try

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        pytest.param(
+            "playgrounds", True,
+            id="Should return True for 'playgrounds'"
+        ),
+        pytest.param(
+            "look", False,
+            id="Should return False for 'look', double 'o'"
+        ),
+        pytest.param(
+            "Adam", False,
+            id="Should return False for 'Adam', double 'a'"
+        ),
+        pytest.param(
+            "", True,
+            id="Should return True for an empty string"
+        ),
+    ],
+)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
